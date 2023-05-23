25a0cbe0a67712e32ee38810eaaf5ebd919496e3
loomsden <loomsden345@gmail.com>
2023-05-22 21:28:59 -0500

    Shrinking columns started but only works on hover and does not change to column on small width

--- JonathanPortfolio/wwwroot/js/site.js
+++ JonathanPortfolio/wwwroot/js/site.js
@@ -105,3 +105,29 @@ class ProjectCard extends HTMLElement {
 }
 
 customElements.define("project-card", ProjectCard);
+
+
+
+/*----------------JS FOR TEST----------------*/
+
+(function () {
+    $(".flex-slide").each(function () {
+        $(this).hover(function () {
+            $(this).find('.flex-title').css({
+                transform: 'rotate(0deg)',
+                top: '10%'
+            });
+            $(this).find('.flex-about').css({
+                opacity: '1'
+            });
+        }, function () {
+            $(this).find('.flex-title').css({
+                transform: 'rotate(90deg)',
+                top: '15%'
+            });
+            $(this).find('.flex-about').css({
+                opacity: '0'
+            });
+        })
+    });
+})();
